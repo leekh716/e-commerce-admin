82326ae912759d76300f41362ae0588dd8d2e1be
kunlee <khlee@ebrain.kr>
2023-10-06 22:56:45 +0900

    feat: link clerk auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,5 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
-  return (
-    <div>
-      <Button size="icon">Click</Button>
-    </div>
-  );
+export default function SetupPage() {
+  return <div></div>;
 }
